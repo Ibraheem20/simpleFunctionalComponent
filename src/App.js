ad08595fcf74931403489513a33fe125515c7dc6
Ibraheem20 <2000.beeeemo@gmail.com>
2022-11-09 19:49:17 +0200

    keep going

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import react,{useState} from 'react';
 
 function App() {
+  const [name, setName]=useState('');
+  const [show,setShow]=useState(false);
+  function onNameChange(e){
+    setName(e.target.value);
+    console.log(name);
+  }
+  function onBtnClicked(e){
+    setShow(!show);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <p>Your Name</p>
+     <input type="text" onChange={onNameChange} value={name} />
+     {name&&!show?
+     <div>
+      hi {name} How are you?
+     </div>
+    :
+    null}
+    {
+      show?
+      <div>
+        Hay {name} Keep going
+      </div>
+      :
+      null
+    }
+    <button onClick={onBtnClicked}>motivate</button>
     </div>
   );
 }
